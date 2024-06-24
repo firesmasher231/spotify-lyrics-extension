73a792d840a36da3747244e806d7c44eae93e05e
firesmasher231 <66473811+firesmasher231@users.noreply.github.com>
2024-06-24 03:04:07 +0100

    added synced highlighting of lyrics with active song, very buggy

--- contentScript copy.js
+++ contentScript copy.js
@@ -25,7 +25,7 @@ async function findAlbum(title, artist) {
 
 		// Find the recording with the closest duration match
 		const recordings = searchData.recordings;
-
+		// console.log(recordings);
 		const recording = recordings.find(
 			(rec) =>
 				// Math.abs(rec.length - duration) < 1000 &&
@@ -33,6 +33,17 @@ async function findAlbum(title, artist) {
 				!rec["releases"][0]["release-group"]["title"].includes("Promo")
 		);
 
+		// console.log(
+		// 	recordings.find(
+		// 		(rec) => !rec["releases"][0]["release-group"]["title"].includes("Apple")
+		// 	)
+		// );
+
+		// const recording = recordings[0];
+		// console.log(
+		// 	recording["releases"][0]["release-group"]["title"].includes("Apple")
+		// );
+		// console.log(recording["releases"][0]["release-group"].id);
 		console.log(recording);
 		if (recording) {
 			// Fetch release groups (albums) for the found recording
@@ -65,7 +76,7 @@ function fetchLyrics(artist, title, callback) {
 			}
 			lastTitle = title;
 
-			// find song duration div
+			// find kQqIrFPM5PjMWb5qUS56
 			songDuration = document
 				.getElementsByClassName("kQqIrFPM5PjMWb5qUS56")[0]
 				.innerText.split(":");
@@ -73,6 +84,11 @@ function fetchLyrics(artist, title, callback) {
 
 			seconds = parseFloat(songDuration[0]) * 60 + parseFloat(songDuration[1]);
 
+			// request.open(
+			// 	"GET",
+			// 	`https://api.lyrics.ovh/v1/Coldplay/Adventure%20of%20a%20Lifetime`
+			// );
+			// url = `https://api.lyrics.ovh/v1/${encodeURI(artist)}/${encodeURI(title)}`;
 			url = `https://lrclib.net/api/get?artist_name=${encodeURI(
 				artist
 			)}&track_name=${encodeURI(title)}&album_name=${encodeURI(
@@ -120,70 +136,6 @@ let isHandlingChanges = false;
 let previousTitle = "";
 let previousArtist = "";
 
-let activeLyric = null;
-
-// Function to sync the lyrics with the song
-function syncLyrics(mutationsList, observer) {
-	// console.log("syncLyrics called");
-	if (!lyricsElement) {
-		console.log("Lyrics element not found");
-		return;
-	}
-
-	const currentTime = document
-		.getElementsByClassName("IPbBrI6yF4zhaizFmrg6")[0]
-		.innerText.split(":");
-	const seconds = parseFloat(currentTime[0]) * 60 + parseFloat(currentTime[1]);
-
-	// Find the active lyric
-	let newActiveLyric = null;
-	for (let child of lyricsElement[0].children) {
-		if (child.getAttribute("data-time") <= seconds) {
-			newActiveLyric = child;
-		} else {
-			break;
-		}
-	}
-
-	// Update the active lyric
-	if (activeLyric !== newActiveLyric) {
-		if (activeLyric) {
-			activeLyric.style.setProperty("color", "white", "important");
-		}
-		if (newActiveLyric) {
-			// newActiveLyric.style.setProperty("color", "#fffa7e", "important");
-			newActiveLyric.style.setProperty("color", "#fd4c46", "important");
-			newActiveLyric.scrollIntoView({ behavior: "smooth", block: "center" });
-			// newActiveLyric.style.setProperty("color", "yellow", "important");
-		}
-		activeLyric = newActiveLyric;
-	}
-}
-
-// call syncLyrics every time the song time changes so oberve the song time element which is the div with class IPbBrI6yF4zhaizFmrg6
-
-let previousTime = "";
-
-let songTimeDiv = document.getElementsByClassName("gglUjikTBtMzCZFgSmpS")[0];
-let songTimeElement = document.getElementsByClassName(
-	"IPbBrI6yF4zhaizFmrg6"
-)[0];
-
-console.log("songTimeElement", songTimeElement);
-
-new MutationObserver(() => {
-	const currentTime = songTimeElement.innerText;
-	// console.log("mutation occured: ", currentTime);
-	if (currentTime !== previousTime) {
-		previousTime = currentTime;
-		syncLyrics();
-	}
-}).observe(songTimeDiv, {
-	subtree: true,
-	childList: true,
-	characterData: true,
-});
-
 // Function to handle changes in the target node
 function handleChanges(mutationsList, observer) {
 	if (isHandlingChanges) return;
@@ -225,7 +177,6 @@ function handleChanges(mutationsList, observer) {
 					);
 
 					if (noOriLyricsEle[0] && !isModLyricsEle[0]) {
-						// ie if the lyrics div is not already created as spotify does not create it for lyrics it doesnt have
 						noOriLyricsEle[0].style.setProperty("display", "none", "important");
 
 						wrapper = document.getElementsByClassName("FUYNhisXTCmbzt9IDxnT");
@@ -261,7 +212,6 @@ function handleChanges(mutationsList, observer) {
 						wrapper[0].appendChild(div);
 					}
 					if (noOriLyricsEle[0]) {
-						// if spotify has lyrics
 						try {
 							noOriLyricsEle[0].style.setProperty(
 								"display",
@@ -290,7 +240,6 @@ function handleChanges(mutationsList, observer) {
 				}
 
 				if (!lyricsElement.length == 0) {
-					// create the lyrics element if it does not exist
 					overlayEle1 = document.getElementsByClassName("hS_lrRHiW4BSWL8WcE8Q");
 					overlayEle2 = document.getElementsByClassName("O7ooKKJG0MArEwDgD6IV");
 
@@ -364,20 +313,15 @@ function handleChanges(mutationsList, observer) {
 							}
 							console.error(error);
 						} else {
+							// console.log("Lyrics:\n", lyrics);
 							currentFullLyrics = lyrics;
+
+							// console.log("currentFullLyrics", currentFullLyrics);
 
 							for (let line of currentFullLyrics.split("\n")) {
 								if (line.includes("Paroles de la chanson")) {
 									continue;
 								}
-
-								// line looks like this   '[01:04.22] I got a lot on my mind', we need to extract the time and the lyrics
-								let time = line.match(/\[(.*?)\]/);
-								if (!time) {
-									continue;
-								}
-								line = line.replace(time[0], "");
-								line = line.trim();
 
 								const div = document.createElement("div");
 								div.setAttribute("dir", "auto");
@@ -387,23 +331,14 @@ function handleChanges(mutationsList, observer) {
 								);
 								div.setAttribute("data-testid", "fullscreen-lyric");
 
-								time = time[0].replace("[", "").replace("]", "");
-
-								let seconds = Math.floor(
-									parseFloat(time.split(":")[0]) * 60 +
-										parseFloat(time.split(":")[1])
-								);
-
-								// add time to the div so that it can be used to sync the lyrics, but convert from [01:04.22] to 64.22
-								// div.setAttribute("data-time", time[0]);
-								div.setAttribute("data-time", seconds);
-
 								const innerDiv = document.createElement("div");
 								innerDiv.setAttribute("class", "BXlQFspJp_jq9SKhUSP3");
 								innerDiv.innerText = line;
 
 								div.appendChild(innerDiv);
 								lyricsElement[0].appendChild(div);
+
+								// console.log("line", line);
 							}
 						}
 					});
@@ -413,17 +348,203 @@ function handleChanges(mutationsList, observer) {
 	}
 	setTimeout(() => {
 		isHandlingChanges = false;
-	}, 100);
+	}, 100); // 2000 milliseconds = 2 seconds
 }
 
+// let processing = false;
+
 function main() {
-	handleChanges([{ type: "childList" }], null);
+	// if (processing) return;
+	// processing = true
+
+	// Configuration object for the observer
+	const config = { childList: true, subtree: true };
+
+	try {
+		noOriLyricsEle = document.getElementsByClassName("vkO5F4KbLk8mbjZoy1Lf");
+
+		if (noOriLyricsEle[0]) {
+			noOriLyricsEle[0].style.setProperty("display", "none", "important");
+
+			wrapper = document.getElementsByClassName("FUYNhisXTCmbzt9IDxnT");
+
+			const div = document.createElement("div");
+			div.setAttribute("class", "gqaWFmQeKNYnYD5gRv3x noOriLyricsEle");
+
+			const innerDiv1 = document.createElement("div");
+			innerDiv1.setAttribute("class", "_Wna90no0o0dta47Heiw");
+			innerDiv1.style.setProperty(
+				"--show-gradient-over-lyrics",
+				"none",
+				"important"
+			);
+
+			const innerDiv2 = document.createElement("div");
+			innerDiv2.setAttribute("dir", "auto");
+			innerDiv2.setAttribute(
+				"class",
+				"nw6rbs8R08fpPn7RWW2w SruqsAzX8rUtY2isUZDF"
+			);
+			innerDiv2.setAttribute("data-testid", "fullscreen-lyric");
+			innerDiv2.style.setProperty("display", "none", "important");
+
+			const innerDiv3 = document.createElement("div");
+			innerDiv3.setAttribute("class", "BXlQFspJp_jq9SKhUSP3");
+
+			innerDiv2.appendChild(innerDiv3);
+			innerDiv1.appendChild(innerDiv2);
+			innerDiv1.appendChild(innerDiv2);
+
+			div.appendChild(innerDiv1);
+			wrapper[0].appendChild(div);
+		}
+	} catch (e) {
+		console.log("Error", e);
+	}
+
+	try {
+		lyricsElement = document.getElementsByClassName("_Wna90no0o0dta47Heiw");
+	} catch (e) {
+		console.log("Error", e);
+		// return;
+	}
+
+	if (!lyricsElement.length == 0) {
+		try {
+			overlayEle1 = document.getElementsByClassName("hS_lrRHiW4BSWL8WcE8Q");
+			overlayEle2 = document.getElementsByClassName("O7ooKKJG0MArEwDgD6IV");
+
+			// set the overlay elements to none
+			overlayEle1[0].style.setProperty("display", "none", "important");
+			overlayEle2[0].style.setProperty("display", "none", "important");
+		} catch (e) {
+			console.log("Error", e);
+		}
+
+		// change --show-gradient-over-lyrics of lyrics element to none
+		lyricsElement[0].style.setProperty(
+			"--show-gradient-over-lyrics",
+			"none",
+			"important"
+		);
+
+		// set all the children of the lyrics element to  none, not deleteing them
+		for (let child of lyricsElement[0].children) {
+			child.style.setProperty("display", "none", "important");
+		}
+
+		// Target node to observe (assume the parent element of the titles)
+		const targetNode = document.getElementsByClassName(
+			"deomraqfhIAoSB3SgXpu"
+		)[0]; // Adjust this to be the closest stable parent element
+
+		// Create an instance of MutationObserver with the callback function
+		const observer = new MutationObserver(handleChanges);
+
+		// Start observing the target node with the specified configuration
+		observer.observe(targetNode, config);
+
+		// Optionally: Handle the initial state if needed
+		const initialTitles = document.getElementsByClassName(
+			"PGSe59fD1Hwc9yUM2d3U"
+		);
+		if (initialTitles.length > 0) {
+			name = initialTitles[0].innerText;
+			artist = initialTitles[1].innerText;
+			console.log("Detected change, new text:", name);
+			title = name;
+
+			if (artist != "") {
+				artist = artist.split(",")[0];
+			}
+			console.log("Detected change, new author:", artist);
+		}
+
+		currentFullLyrics = "";
+
+		// Fetch lyrics for the song
+		fetchLyrics(artist, title, (error, lyrics) => {
+			if (error) {
+				console.error(error, lyrics);
+				if (error == "Error fetching lyrics") {
+					console.log("Error fetching lyrics");
+					const div = document.createElement("div");
+					div.setAttribute("dir", "auto");
+					div.setAttribute(
+						"class",
+						"nw6rbs8R08fpPn7RWW2w SruqsAzX8rUtY2isUZDF"
+					);
+					div.setAttribute("data-testid", "fullscreen-lyric");
+
+					const innerDiv = document.createElement("div");
+					innerDiv.setAttribute("class", "BXlQFspJp_jq9SKhUSP3");
+					innerDiv.innerText = "Error fetching lyrics, sorry! :(";
+
+					div.appendChild(innerDiv);
+					lyricsElement[0].appendChild(div);
+				}
+			} else {
+				console.log("Lyrics:\n", lyrics);
+				currentFullLyrics = lyrics;
+
+				// console.log("currentFullLyrics", currentFullLyrics);
+
+				for (let line of currentFullLyrics.split("\n")) {
+					if (line.includes("Paroles de la chanson")) {
+						continue;
+					}
+
+					const div = document.createElement("div");
+					div.setAttribute("dir", "auto");
+					div.setAttribute(
+						"class",
+						"nw6rbs8R08fpPn7RWW2w SruqsAzX8rUtY2isUZDF"
+					);
+					div.setAttribute("data-testid", "fullscreen-lyric");
+
+					line = line.trim();
+					// console.log("line", line.substring(0, 1), line.substring(-1, -1));
+
+					if (
+						(line.substring(0, 1) == "[" && line.substring(-1, -1) == "]") ||
+						(line.substring(0, 1) == "(" && line.substring(-1, -1) == ")")
+					) {
+						console.log("line", line);
+						div.style.color = rgb(117, 117, 134);
+					}
+
+					const innerDiv = document.createElement("div");
+					innerDiv.setAttribute("class", "BXlQFspJp_jq9SKhUSP3");
+					innerDiv.innerText = line;
+
+					div.appendChild(innerDiv);
+					lyricsElement[0].appendChild(div);
+				}
+			}
+		});
+	}
+	processing = false;
 }
+
+// Function to convert prices on the page
 
 function runScript() {
 	console.log("ran");
-	main();
+	// sleep for 2 seconds
+	setTimeout(() => {
+		// get children of lyrics
+		// console.log(lyrics[0].children);
+		// get text of lyrics and print the loaded partial lyrics
+		// text = lyrics[0].innerText;
+		// console.log;
+		// console.log("Lyrics:\n", text);
+
+		main();
+	}, 2000);
 }
+
+// Call the price conversion function when the page has finished loading
+// window.addEventListener("load", runScript);
 
 // Function to detect URL changes
 function handleUrlChange() {
@@ -459,4 +580,3 @@ new MutationObserver(() => {
 // Initial check
 
 window.addEventListener("load", handleUrlChange());
-// window.addEventListener("load", startObservingSongTime());
